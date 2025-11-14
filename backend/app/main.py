6118c4f1e3127316712b377190117ad81920ab82
WetMix <sereza2044@gmail.com>
2025-11-14 15:11:10 +0300

    обновил docker-cmpose.langfuse.yml и обновил ci-cd.yml

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 from langfuse import Langfuse
+from fastapi import FastAPI
 import os
+app = FastAPI()
 langfuse = Langfuse(
     public_key=os.getenv("LANGFUSE_PUBLIC_KEY"),
     secret_key=os.getenv("LANGFUSE_SECRET_KEY"),
     host=os.getenv("LANGFUSE_HOST"),
 )
+
 
 @app.middleware("http")
 async def langfuse_trace(request, call_next):
