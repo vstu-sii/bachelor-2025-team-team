63163ae2b17cf88ed5d059eaad73596510ebe54e
WetMix <sereza2044@gmail.com>
2025-11-14 15:02:04 +0300

    обновил docker-cmpose.langfuse.yml и обновил ci-cd.yml

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,4 +1,5 @@
 import js from "@eslint/js";
+import react from "eslint-plugin-react";
 import globals from "globals";
 
 export default [
@@ -11,9 +12,18 @@ export default [
       ecmaVersion: "latest",
       sourceType: "module",
       globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      react
     },
     rules: {
       semi: "warn",
-    },
+      "react/react-in-jsx-scope": "off" // если используешь Next.js / React 17+
+    }
   },
 ];
